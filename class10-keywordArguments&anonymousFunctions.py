f84fef45f45f8aa8d6734b849add718395417771
Vipin <vkprajapati665@gmail.com>
2022-12-11 23:40:46 +0530

    Class10

--- class10-keywordArguments&anonymousFunctions.py
+++ class10-keywordArguments&anonymousFunctions.py
@@ -9,5 +9,18 @@ def func(a,b,c):
     print(b)
     print(c)
 func(c=1,a=2,b= 3)    
+
+
+def func(a,b,*c):
+    print(a)
+    print(b)
+    print(c)
     
-print?    
+func(1,2,3,4,5)    
+
+def func(**k):
+    print(k)
+func(name="Jatin")    
+
+x=lambda a,b: a*b
+print(x(1,2))
