85cfa6a458e2117e4daec875006dc819869e606c
Vipin <vkprajapati665@gmail.com>
2022-12-11 22:55:58 +0530

    class10

---
+++ class10-keywordArguments&anonymousFunctions.py
@@ -0,0 +1,13 @@
+def func(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+func(1,2,3)    
+    
+def func(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+func(c=1,a=2,b= 3)    
+    
+print?    
